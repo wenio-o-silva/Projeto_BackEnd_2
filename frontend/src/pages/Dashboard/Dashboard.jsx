9cb7a8930159ac47911d0c86ad1f48c888993283
Wenio Silva <weniosilva@alunos.utfpr.edu.br>
2025-12-09 00:28:12 -0300

    Estrutura FrontEnd

---
+++ frontend/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+import styles from "./Dashboard.module.css";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World do Dashboard!</h1>
+    </div>
+  );
+}
